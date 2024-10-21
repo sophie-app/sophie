0cdace60db66e68e79c3f910a2f797a822e2d055
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-10-22 01:00:09 +0900

    fix(backend): api error in endpoint of transription

--- apps/backend/src/usecases/audioUseCase.ts
+++ apps/backend/src/usecases/audioUseCase.ts
@@ -3,7 +3,7 @@ import { base64ToFile } from '../utils/base64ToFile'
 
 export const audioUseCases = {
   createTranscription: async (fileBase64: string, openaiClient: OpenAI) => {
-    const audioFile = base64ToFile(fileBase64, 'audio.mp3', 'audio/mp3')
+    const audioFile = base64ToFile(fileBase64, 'audio.wav', 'audio/wav')
     const { text } = await openaiClient.audio.transcriptions.create({
       file: audioFile,
       model: 'whisper-1',
